a846c1b8852cffeccfc6861a616ce4088ccbd10c
Anju <anjumpkr4115@gmail.com>
2025-01-15 17:05:43 +0530

    image uploading

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,61 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useRef,useState,useEffect } from 'react';
+import axios from 'axios'
 
 const Profile = () => {
+  const fileRef=useRef(null);
   const {currentUser}=useSelector((state)=>state.user)
+  const [image,setImage]=useState(undefined)
+  const [formData,setFormData]=useState({})
+  const [imageError,setImageError]=useState("")
+   
+  const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dihyb66hn/image/upload";
+  const CLOUDINARY_UPLOAD_PRESET = "life-mern";
+
+
+  console.log(image)
+  useEffect(()=>{
+    if(image){
+      uploadImageToCloudinary(image)
+    }
+  },[image])
+
+
+  const uploadImageToCloudinary = async (image) => {
+    const formImage = new FormData();
+        formImage.append("file", image);  
+        formImage.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+    try {
+        console.log(formData)
+        const response = await axios.post(CLOUDINARY_URL, formImage);
+        console.log("helllllo")
+        const imageUrl=response.data.secure_url;
+        console.log(imageUrl)
+        setFormData({...formData,profilPicture:imageUrl})
+        console.log(formData)
+    } catch (error) {
+        console.error("Cloudinary upload failed", error);
+        setImageError("Image upload failed!");
+      
+    }
+};
+
 
   return (
     <div className="p-3 max-w-lg mx-auto ">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
+        <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e)=>setImage(e.target.files[0])} />
         <img 
         src={currentUser.profilePicture}
         alt="profile"
         className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"
+        onClick={()=>fileRef.current.click()}
         />
+
+        <p className='text-red-700'>
+          {imageError}</p>
         <input defaultValue={currentUser.username }type="text" id='username'
         placeholder='Username' className='bg-slate-100 rounded-lg p-3'/>
         <input defaultValue={currentUser.email}type="email" id='email'
