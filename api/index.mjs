3e3a83a3aca818b8f181ce7a9f27305364e7a713
Anju <anjumpkr4115@gmail.com>
2025-01-16 21:14:07 +0530

    add preproduction changes

--- api/index.mjs
+++ api/index.mjs
@@ -5,6 +5,7 @@ import userRoutes from './routes/userRoute.mjs'
 import authRoutes from './routes/authRoute.mjs'
 import adminRoutes from './routes/adminRoute.mjs'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
@@ -12,7 +13,13 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 }).catch((err)=>{
     console.log(err)
 })
+const __dirname=path.resolve();
 const app=express();
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use(express.json());
 app.use(cookieParser());
 
